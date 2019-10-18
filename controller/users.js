096a998bb3c77f8a091de2ad2a547fff490b2325
Jesseliz <choque101108@gmail.com>
2019-10-18 18:03:58 -0500

    method put and delete

--- controller/users.js
+++ controller/users.js
@@ -3,19 +3,29 @@
 const users = require("../model/modelUsers");
 const bcrypt = require("bcrypt");
 
+uidOrEmail = (param) => {
+  console.log('uidOrEmail', uidOrEmail);
+  let obj = new Object();
+  if (param.indexOf('@') < 0) {
+    obj._id = param;
+  } else {
+    obj.email = param;
+  }
+  return obj
+}
+
 module.exports.getUsers = async (req, resp) => {
-  
   try {
     const result = await users.find().exec();
     resp.send(result);
   } catch (error) {
     resp.status(500).send(error);
-}
+  }
 };
 
 module.exports.getUserId = async (req, resp) => {
-  
- console.log(req);
+
+  console.log(req);
 };
 
 module.exports.postUser = async (req, resp, next) => {
@@ -23,10 +33,8 @@ module.exports.postUser = async (req, resp, next) => {
     return next(400);
   }
   const userInvalid = await users.findOne({ email: req.body.email });
-  console.log("debug es user validate", userInvalid); //null
   if (userInvalid) return next(403);
   let newUser = new users();
-  console.log("debug user new user", newUser); //null
   newUser.email = req.body.email;
   newUser.password = bcrypt.hashSync(req.body.password, 10);
   if (req.body._id) {
@@ -40,26 +48,22 @@ module.exports.postUser = async (req, resp, next) => {
     roles: userStored.roles,
     _id: userStored._id.toString(),
     email: userStored.email
-});
+  });
 };
 
 module.exports.putUser = async (req, resp, next) => {
   try {
+    console.log('aquii 1', !isAdmin(req) && req.body.roles);
     if (!isAdmin(req) && req.body.roles) {
       return next(403);
-    }
-    if (!req.body.email && !req.body.password && !isAdmin(req)) {
-      return next(400);
-    }
+    };
 
-    // const obj = uidOrEmail(req.params.uid);
-
+    console.log('aquii 2 if', !req.body.email && !req.body.password && !isAdmin(req));
     if (!req.body.email && !req.body.password && !isAdmin(req)) {
       return next(400)
     }
-    
-    obj = uidOrEmail(req.params.uid);
-
+    let obj = uidOrEmail(req.params.uid);
+    console.log('obj de uidOr Email', obj);
     const userFounded = await users.findOne(obj);
     if (req.body.email) {
       userFounded.email = req.body.email;
@@ -74,4 +78,19 @@ module.exports.putUser = async (req, resp, next) => {
   } catch (e) {
     return next(404)
   }
-}
+};
+
+module.exports.deleteUser = async (req, resp, next) => {
+  try {
+    const obj = uidOrEmail(req.params.uid);
+    const userdeleted = await users.findOne(obj)
+    if (!userdeleted) {
+      return next(404)
+    }
+    const userRemoved = await users.remove(obj);
+    return resp.send({ message: 'Se borro satisfactoriamente!' });
+
+  } catch (e) {
+    return next(404)
+  }
+};
