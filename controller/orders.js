2a3666b0e1b63372422831d06181077cf29c5fd2
Jesseliz <choque101108@gmail.com>
2019-10-30 18:18:44 -0500

    post orders

--- controller/orders.js
+++ controller/orders.js
@@ -1,36 +1,31 @@
 /* eslint-disable require-atomic-updates */
-const order = require('../model/modelOrders')
 const products = require('../model/modelProducts');
+const order = require('../model/modelOrders');
 // const pagination = require('../utils/pagination');
 const mongodb = require('mongodb');
 
 module.exports.postOrders = async (req, resp, next) => {
-    if (!req.body.products || req.body.products.length === 0) {
-        //no se indica `userId` o se intenta crear una orden sin productos
-        return next(400);
-    }
-    let newOrder = new order();
-    newOrder.dateEntry = Date.now();
-    newOrder.userId = req.headers.user._id;
-    newOrder.client = req.body.client;
-    const arrProducts = await products.find({ _id: { $in: req.body.products.map(pId => mongodb.ObjectId(pId.product)) } })
-    if (arrProducts.length !== req.body.products.length) {
-        req.body.products = req.body.products.filter((x) => {
-            //filtra todos los productos que sean diferentes de null
-            return x !== null || undefined;
-        })
-    }
-    const allProducts = req.body.products.map((currentProduct, index) => ({
-        product: {
-            _id: currentProduct.product,
-            name: arrProducts[index].name,
-            price: arrProducts[index].price,
-        },
-        qty: currentProduct.qty
-    }));
+    try {
+        if (!req.body.products || req.body.products.length === 0) {
+            //no se indica `userId` o se intenta crear una orden sin productos
+            return next(400);
+        }
+        let newOrder = new order();
+        newOrder.dateEntry = Date.now();
+        newOrder.userId = req.headers.user._id;
+        newOrder.client = req.body.client;
 
-    newOrder.products = allProducts;
-    const orderStored = await newOrder.save();
-    return resp.send(orderStored)
-};
+        const arrProducts = await products.find({ _id: { $in: req.body.products.map(pId => mongodb.ObjectId(pId.product)) } })
+        console.log('array de products test', arrProducts);
+        const allProducts = req.body.products.map((currentProduct, index) => ({
+            product: arrProducts.find(p => p._id.equals(currentProduct._id)),
+            qty: currentProduct.qty
+        }));
 
+        newOrder.products = allProducts;
+        const orderStored = await newOrder.save();
+        return resp.send(orderStored)
+    } catch (error) {
+        console.error(error)
+    }
+}
